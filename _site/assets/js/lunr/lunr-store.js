daf70e2179988c9c722cf0046c93a94cedee1aa8
Sungbin710 <59648766+Sungbin710@users.noreply.github.com>
2021-03-15 19:37:31 +0900

    start

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "launching blog!",
+        "excerpt":"blog를 시작합니다!   ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      }]
